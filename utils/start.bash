9a753edc0bb3514c414a921e232b5b3c15c3ef1c
JiayeZhu <1352953@tongji.edu.cn>
2019-10-25 22:55:02 -0400

    update start stricpt

--- utils/start.bash
+++ utils/start.bash
@@ -1,7 +1,7 @@
-python3 CentralizedVersion/server.py > server.log &
+python3 CentralizedVersion/server.py -T $4 > server.log &
 
 for((i = 0; i < $1; i++))
 do
-	python3 CentralizedVersion/client.py -p $[23333+$i] -s 127.0.0.1:8888 -i sharing_$[0+$i] -o downloads_$[0+$i] -N $2 -f $3 > client_$[0+$i].log &
+	python3 CentralizedVersion/client.py -p $[23333+$i] -s 127.0.0.1:8888 -i sharing_$[0+$i] -o downloads_$[0+$i] -N $2 -f $3  -T $4 > client_$[0+$i].log &
 done
 
